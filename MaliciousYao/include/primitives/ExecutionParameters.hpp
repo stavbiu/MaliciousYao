9de815f9b28cfe4c88e096d58585d7e5ffe543a1
stavbiu <stav.biu@gmail.com>
2016-05-18 15:20:46 +0300

    Crate ExecutionParameters

--- MaliciousYao/include/primitives/ExecutionParameters.hpp
+++ MaliciousYao/include/primitives/ExecutionParameters.hpp
@@ -35,7 +35,19 @@ public:
 	 Constructor that sets the parameters.
 	*/
 	ExecutionParameters(GarbledBooleanCircuit* bc, GarbledBooleanCircuit* mainGbc, 
-		int numExecutions, int statisticalParameter, int bucketSize, double evaluationProbability);
+		int numExecutions, int statisticalParameter, int bucketSize, double evaluationProbability)
+	{
+		this->bc = bc;
+		this->gbc = mainGbc;
+		this->numExecutions = numExecutions;	//N
+		this->statisticalParameter = statisticalParameter;	//s
+		this->bucketSize = bucketSize;	//B
+		this->evaluationProbability = evaluationProbability; //P
+
+		this->evalCircuits = numExecutions * bucketSize;
+		this->numCircuits = ceil(evalCircuits / evaluationProbability);
+		this->checkCircuits = numCircuits - evalCircuits;
+	}
 
 	/*
 	 Getters
